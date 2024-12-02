b71906176c9ce0abc4e5fb141c74cf9a4d96a178
Osama23Szabist <bsse2380125@szabist.pk>
2024-12-02 20:05:30 +0500

    osama

--- Thecompressor.py
+++ Thecompressor.py
@@ -3,30 +3,17 @@ with open ("story.txt","r",encoding='utf-8') as file:#reading text file
     text = file.read()
 
 word = ""
-listofwords = []
+listofwords = linkList.sll()
 wordCounter = 0
 
 for i in range(0,len(text)):#breaking it down word by word
     if " " in text[i]:
         if len(word) > 0:
-            flag = True
-            for info in listofwords:
-                if info[1] in word:
-                    flag = False
-                    break
+            flag, Thenode = listofwords.search(word)
             if flag:
-                listofwords.append((1,word,i))
+                listofwords.mod(Thenode,i)
             else:
-                for k in range(0,len(listofwords)):
-                    if listofwords[k][1] in word:
-                        num = listofwords[k][0] +1
-                        locs = listofwords[k][2]
-                        if isinstance(locs, list):
-                            locs.append(i)
-                        else:
-                            locs = locs , i
-                        listofwords.remove(listofwords[k])
-                        listofwords.append((num,word,locs))
+                listofwords.insert(word,i)
         word = ""
         wordCounter += 1
     else:
@@ -34,15 +21,11 @@ for i in range(0,len(text)):#breaking it down word by word
 
 
 newlist = linkList.sll()
-listofwords.sort(reverse=True)
 
-for i in range(0,len(listofwords)):#calci=ualting to see which word needs to be removed 
+"""for i in range(0,len(listofwords)):#calci=ualting to see which word needs to be removed 
     if ((len(listofwords[i][1])*8) * listofwords[i][0]) >(len(listofwords[i][1])*8)+10:#need to refine this here
-        newlist.insert(listofwords[i])
+        newlist.insert(listofwords[i])"""
 
-newlist.print()
-print(len(listofwords))
+listofwords.print()
 
-for i in range(3102-len("Python,"),3102):#error unable to recontruct
-    print(text[i])
     
--- linkList.py
+++ linkList.py
@@ -1,14 +1,17 @@
 class sll:
     class node:
-        def __init__(self,data):
+        def __init__(self,data,loc):
+            self.count = 1
             self.data = data
             self.next = None
-            self.loc= []
+            self.loc= [loc]
+            self.worthit = False
     def __init__(self):
         self.head = None
+        self.bitsaved = 0
 
-    def insert (self,data):
-        newnode = self.node(data)
+    def insert (self,data,loc):
+        newnode = self.node(data,loc)
         if self.head == None:
             self.head = newnode
             return
@@ -16,12 +19,33 @@ class sll:
         while (temp.next != None):
             temp = temp.next
         temp.next = newnode
+    
+    def mod(self,nodeloc,loc):
+        nodeloc.loc.append(loc)
+        nodeloc.count += 1
+        ogsize = (len(nodeloc.data)*8)*nodeloc.count
+        newsize = 0
+        for data in nodeloc.loc:
+            newsize += data.bit_length()
+        newsize += (len(nodeloc.data)*8)
+        if (ogsize)>(newsize):
+            nodeloc.worthit = True
+            self.bitsaved += ogsize-newsize
+
+    def search(self,data):
+        temp = self.head
+        while(temp != None):
+            if temp.data in data:
+                return True, temp
+            temp = temp.next
+        return False, None
+            
 
     def print(self):
         temp = self.head
         count = 0
         while (temp != None):
-            print(temp.data)
+            print("word:",temp.data,"count:",temp.count,"locs:",temp.loc,"wroth:",temp.worthit)
             temp = temp.next
             count+= 1
-        print("total size",count)
+        print("total size",count,"bitsaved:",self.bitsaved)
