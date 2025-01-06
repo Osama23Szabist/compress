be02695b6195d5916a89b250ae7462660a09fc5e
Osama23Szabist <bsse2380125@szabist.pk>
2025-01-06 13:53:29 +0500

    osama

--- Avl.py
+++ Avl.py
@@ -4,6 +4,7 @@ class tree:
             self.data = data
             self.left = None
             self.right = None
+            self.pre = None
             self.count = 1
             self.worthit = False
             self.inPreList = False
@@ -11,6 +12,27 @@ class tree:
     def __init__(self,prelist):
         self.prelist = prelist
         self.root = None
+
+    def balance(self,thenode,rh,lh):
+        if (rh-lh) > 1:
+            pass 
+        elif (rh-lh) < -1:
+            pass
+
+
+    def heights(self,thenode,h = 0):
+        if thenode.right != None and thenode.left != None:
+            h += 1
+            h = self.heights(thenode.right,h)
+            h = self.heights(thenode.left,h)
+        else:
+            if thenode.right != None:
+                h += 1
+                h = self.heights(thenode.right,h)
+            elif thenode.left != None:
+                h += 1
+                h = self.heights(thenode.left,h)
+        return h
 
     def insert(self,data):
         ans = self.search(data)
@@ -20,7 +42,21 @@ class tree:
             while temp != None:
                 if temp.data > data:
                     if temp.left == None:
+                        newnode.pre = temp
                         temp.left = newnode
+                        break
+                    else:
+                        temp = temp.left
+                else:
+                    if temp.right == None:
+                        newnode.pre = temp
+                        temp.right = newnode
+                        break
+                    else:
+                        temp = temp.right
+            while temp != self.root:
+                self.balance(temp,self.heights(temp.right),self.heights(temp.right))
+                temp = temp.pre
 
     def search(self,data):
         temp = self.root
